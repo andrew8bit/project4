8b38f9a463f4a3e4e781013023312353a3324bfa
Ana Ceprnja <aceprnja@iun.edu>
2021-04-01 15:48:34 -0600

    donate done

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     
     path('accounts/signup', views.signup, name='signup'),
     path('accounts/logout', views.logout_view, name='logout_view'),
-    path('donations/', views.donation_view, name="donation_view")
+    path('donation/', views.donation, name="donation"),
+    path('charge/', views.charge, name="charge"),
+    path('thankyou/<str:args>/', views.thankyouMsg, name="thankyou"),
 
 ]
--- project4/settings.py
+++ project4/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+import environ
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=-(^k5k(+i1ujyt)qppy3%g0bqsazdz_^=gg8u4zz!*pnvf4h6'
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,6 +125,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-LOGOUT_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL = '/'
 
-LOGIN_REDIRECT_URL='/'
+LOGIN_REDIRECT_URL = '/'
