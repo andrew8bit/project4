b0bb20b3771761e252918f3973899cf41d64470d
Ana Ceprnja <aceprnja@iun.edu>
2021-04-01 15:54:27 -0600

    donation

--- project4/settings.py
+++ project4/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
