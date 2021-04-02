59e7169009845771ace221c026657ee90a8c6757
Andrew8bit <Andrewbith@gmail.com>
2021-04-02 12:26:43 -0700

    collect static files - fixed logo png - set nav base shop

--- main_app/views.py
+++ main_app/views.py
@@ -78,8 +78,7 @@ def index(request):
 
 def logout_view(request):
     logout(request)
-
-
+    
 
 def donation(request):
     return render(request, 'donation.html')
@@ -118,3 +117,6 @@ def thankyouMsg(request, args):
     amount = args
     return render(request, 'thankyou.html', {'amount': amount})
 
+def bounty_post(request):
+
+  return render(request, 'bounty_post.html')
