84f1333c1ae26a983bd1f08ca1b4735be518958c
Andrew8bit <Andrewbith@gmail.com>
2021-04-04 01:17:33 -0700

    added simple notification messages

--- project4/settings.py
+++ project4/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import environ
 from pathlib import Path
-import os
-from pathlib import Path
 from dotenv import load_dotenv
+from django.contrib.messages import constants as messages
+
+
 load_dotenv()
 
 stripe_API_KEY = os.getenv('STRIPE_KEY')
@@ -134,3 +135,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGOUT_REDIRECT_URL = '/'
 
 LOGIN_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+    
+}
